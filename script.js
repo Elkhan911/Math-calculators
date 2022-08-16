674db00ec9a063a7ab8fa29f1080e0c020a00484
Elkhan911 <955pro@mail.ru>
2022-08-17 00:20:36 +0500

    created files

---
+++ script.js
@@ -0,0 +1,11 @@
+// main script for take the sums of square roots
+function sumsOfSquareRoots(a, b, c) {
+  let result = Math.ceil(Math.sqrt(a) + Math.sqrt(b) + Math.sqrt(c));
+  return result;
+}
+
+// console.log(sumsOfSquareRoots(4, 4, 4));
+
+function isItPythagoreanTriple(a, b, c) {}
+
+console.log(Math.max.call());
