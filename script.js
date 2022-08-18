26c83298eaccc924a4d5edef21beca61caf3b82d
Elkhan911 <955pro@mail.ru>
2022-08-18 09:39:39 +0500

    functional is working

--- script.js
+++ script.js
@@ -1,69 +1,95 @@
-let input1 = document.querySelector("#input1");
-let input2 = document.querySelector("#input2");
-let input3 = document.querySelector("#input3");
-let ParentTaskAnswerZone = document.querySelector("#ParentTaskAnswerZone");
-let button1 = document.querySelector("#button1");
-let buttonReset = document.querySelector("#buttonReset");
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const input3 = document.querySelector("#input3");
+const ParentTaskAnswerZone = document.querySelector("#ParentTaskAnswerZone");
+const takeAnswerBtn = document.querySelector("#takeAnswerBtn");
+const buttonReset = document.querySelector("#buttonReset");
+
+const taskAnswerP1 = document.querySelector("#taskAnswerP1");
+const taskAnswerP2 = document.querySelector("#taskAnswerP2");
+const taskAnswerP3 = document.querySelector("#taskAnswerP3");
+const taskAnswerMain = document.querySelector("#taskAnswerMain");
 
 //******************************************************************************
 //******************************* VARIABLES ************************************
 
-// +- функция для определения Пифагоровой тройки
+// получаем зачение первого инпута и сохраняем в span
+input1.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    taskAnswerP1.textContent = "Первое значение: ";
+    let span = document.createElement("span");
+    span.textContent = " " + this.value;
+    span.id = "span1";
+    taskAnswerP1.append(span);
+    this.value = "";
+  }
+});
+
+// получаем зачение второго инпута и сохраняем в span
+input2.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    taskAnswerP2.textContent = "Второе значение: ";
+    let span = document.createElement("span");
+    span.textContent = " " + this.value;
+    span.id = "span2";
+    taskAnswerP2.append(span);
+    this.value = "";
+  }
+});
+
+// получаем зачение третьего инпута и сохраняем в span
+input3.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    taskAnswerP3.textContent = "Третье значение: ";
+    let span = document.createElement("span");
+    span.textContent = " " + this.value;
+    span.id = "span3";
+    taskAnswerP3.append(span);
+    this.value = "";
+  }
+});
+
+// функция для определения Пифагоровой тройки
 function isItPythagoreanTriple(a, b, c) {
-  let aSquare = Math.pow(a, 2);
-  let bSquare = Math.pow(b, 2);
-  let cSquare = Math.pow(c, 2);
+  const aSquare = Math.pow(a, 2);
+  const bSquare = Math.pow(b, 2);
+  const cSquare = Math.pow(c, 2);
 
-  console.log(aSquare, bSquare, cSquare);
-
-  let max = Math.max(aSquare, bSquare, cSquare);
-  console.log(max);
-
+  const MaxValue = Math.max(aSquare, bSquare, cSquare);
   if (
-    max == aSquare + bSquare ||
-    max == aSquare + cSquare ||
-    max == bSquare + cSquare
+    MaxValue == aSquare + bSquare ||
+    MaxValue == aSquare + cSquare ||
+    MaxValue == bSquare + cSquare
   ) {
     return true;
   } else return false;
 }
 
-// console.log(isItPythagoreanTriple(4, 3, 5));
+// определяем по клику Пифагоровая ли это тройка и сохраяем результат в span
+takeAnswerBtn.addEventListener("click", function () {
+  const a = Number(span1.textContent);
+  const b = Number(span2.textContent);
+  const c = Number(span3.textContent);
 
-input1.addEventListener("blur", func1);
-
-function func1() {
-  let p = document.createElement("p");
-  p.textContent = "Первое значение: " + this.value;
-  ParentTaskAnswerZone.append(p);
-
-  input1.removeEventListener("blur", func1);
-}
-
-input2.addEventListener("blur", function () {
-  let p = document.createElement("p");
-  p.textContent = "Второе значение: " + this.value;
-  ParentTaskAnswerZone.append(p);
-});
-
-input3.addEventListener("blur", function () {
-  let p = document.createElement("p");
-  p.textContent = "Третье значение: " + this.value;
-  ParentTaskAnswerZone.append(p);
-});
-
-button1.addEventListener("click", function () {
-  if (isItPythagoreanTriple(input1.value, input2.value, input3.value)) {
-    taskAnswer1.textContent += "Да, это Пифагорова тройка";
+  if (isItPythagoreanTriple(a, b, c) == true) {
+    let spanAnswer = document.createElement("span");
+    spanAnswer.id = "spanAnswer";
+    spanAnswer.textContent = "Да, это Пифагоровая тройка";
+    taskAnswerMain.append(spanAnswer);
   } else {
-    taskAnswer1.textContent += "Нет, это не Пифагорова тройка";
+    let spanAnswer = document.createElement("span");
+    spanAnswer.id = "spanAnswer";
+    spanAnswer.textContent = "Нет, это не Пифагровая тройка";
+    taskAnswerMain.append(spanAnswer);
   }
 });
 
-buttonReset.addEventListener("click", function () {
-  input1.value = "";
-  input2.value = "";
-  input3.value = "";
+// функция сброса
+function resetValues() {
+  span1.textContent = "";
+  span2.textContent = "";
+  span3.textContent = "";
+  spanAnswer.textContent = "";
+}
 
-  taskAnswer1.textContent = "Введеные значения являеются Пифагоровой тройкой?";
-});
+buttonReset.addEventListener("click", resetValues);
