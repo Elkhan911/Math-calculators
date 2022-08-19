06e15e8ab38b87b02ab83fc70e86602e8dd8e235
Elkhan911 <955pro@mail.ru>
2022-08-19 16:14:58 +0500

    fix problems with js variables

--- script.js
+++ script.js
@@ -1,86 +1,80 @@
 // Variables of Task one
-const input1 = document.querySelector("#input1Task1");
-const input2 = document.querySelector("#input2Task1");
-const input3 = document.querySelector("#input3Task1");
-const parentTaskAnswerZone = document.querySelector("#parentTaskAnswerZone");
-const takeAnswerBtn1 = document.querySelector("#takeAnswerBtn1");
-const buttonReset1 = document.querySelector("#buttonReset1");
-const taskAnswerP1 = document.querySelector("#taskAnswerP1");
-const taskAnswerP2 = document.querySelector("#taskAnswerP2");
-const taskAnswerP3 = document.querySelector("#taskAnswerP3");
-const taskAnswerMain = document.querySelector("#taskAnswerMain");
+const input1Task1 = document.querySelector("#_input1Task1");
+const input2Task1 = document.querySelector("#_input2Task1");
+const input3Task1 = document.querySelector("#_input3Task1");
+const takeAnswerBtn1 = document.querySelector("#_takeAnswerBtn1");
+const buttonReset1 = document.querySelector("#_buttonReset1");
+const span1 = document.querySelector("#_span1");
+const span2 = document.querySelector("#_span2");
+const span3 = document.querySelector("#_span3");
+const spanAnswer = document.querySelector("#_spanAnswer");
 
 // Variables of Task two
-const input21 = document.querySelector("#input1Task2");
-const taskAnswerMain2 = document.querySelector("#taskAnswerMainTask2");
-const takeAnswerBtn2 = document.querySelector("#takeAnswerBtn2");
-const buttonReset2 = document.querySelector("#buttonReset2");
+const input1Task2 = document.querySelector("#_input1Task2");
+const taskAnswerMain2 = document.querySelector("#_taskAnswerMainTask2");
+const takeAnswerBtn2 = document.querySelector("#_takeAnswerBtn2");
+const buttonReset2 = document.querySelector("#_buttonReset2");
 const parentTaskAnswerZoneTask2 = document.querySelector(
   "#parentTaskAnswerZoneTask2"
 );
+const span1Task2 = document.querySelector("#_span1Task2");
+const spanAnswerTask2 = document.querySelector("#_spanAnswerTask2");
 
 // Variables of Task three
-const takeAnswerBtn3 = document.querySelector("#takeAnswerBtn3");
-const buttonReset3 = document.querySelector("#buttonReset3");
+const takeAnswerBtn3 = document.querySelector("#_takeAnswerBtn3");
+const buttonReset3 = document.querySelector("#_buttonReset3");
 const parentTaskAnswerZoneTask3 = document.querySelector(
-  "#parentTaskAnswerZoneTask3"
+  "#_parentTaskAnswerZoneTask3"
 );
-const AnswerP1Task3 = document.querySelector("#AnswerP1Task3");
-const AnswerP2Task3 = document.querySelector("#AnswerP2Task3");
-const taskAnswerMainTask3 = document.querySelector("#taskAnswerMainTask3");
-const input1Task3 = document.querySelector("#input1Task3");
-const input2Task3 = document.querySelector("#input2Task3");
+const AnswerP1Task3 = document.querySelector("#_AnswerP1Task3");
+const AnswerP2Task3 = document.querySelector("#_AnswerP2Task3");
+const taskAnswerMainTask3 = document.querySelector("#_taskAnswerMainTask3");
+const input1Task3 = document.querySelector("#_input1Task3");
+const input2Task3 = document.querySelector("#_input2Task3");
+const span31 = document.querySelector("#_span31");
+const span32 = document.querySelector("#_span32");
 
 // Variables of Task four
-const _input1Task4 = document.querySelector("#input1Task4");
-const _input2Task4 = document.querySelector("#input2Task4");
-const _takeAnswerBtn4 = document.querySelector("#takeAnswerBtn4");
-const _buttonReset4 = document.querySelector("#buttonReset4");
-const _parentTaskAnswerZoneTask4 = document.querySelector(
-  "#parentTaskAnswerZoneTask4"
+const input1Task4 = document.querySelector("#_input1Task4");
+const input2Task4 = document.querySelector("#_input2Task4");
+const takeAnswerBtn4 = document.querySelector("#_takeAnswerBtn4");
+const buttonReset4 = document.querySelector("#_buttonReset4");
+const parentTaskAnswerZoneTask4 = document.querySelector(
+  "#_parentTaskAnswerZoneTask4"
 );
-const _AnswerP1Task4 = document.querySelector("#AnswerP1Task4");
-const _AnswerP2Task4 = document.querySelector("#AnswerP2Task4");
-const _taskAnswerMainTask4 = document.querySelector("#taskAnswerMainTask4");
-const _span41 = document.querySelector("#span41");
-const _span42 = document.querySelector("#span42");
-const _spanMainAnswer = document.querySelector("#spanMainAnswer");
+const AnswerP1Task4 = document.querySelector("#_AnswerP1Task4");
+const AnswerP2Task4 = document.querySelector("#_AnswerP2Task4");
+const taskAnswerMainTask4 = document.querySelector("#_taskAnswerMainTask4");
+const span41 = document.querySelector("#_span41");
+const span42 = document.querySelector("#_span42");
+const spanMainAnswer = document.querySelector("#_spanMainAnswer");
 
 //******************************************************************************
 //******************************* VARIABLES ************************************
 
+//******************************************************************************
+//******************************* TASK ONE  ************************************
+
 // получаем зачение первого инпута и сохраняем в span
-input1.addEventListener("keydown", function (event) {
+input1Task1.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    taskAnswerP1.textContent = "Первое значение: ";
-    let span = document.createElement("span");
-    span.textContent = " " + this.value;
-    span.id = "span1";
-    taskAnswerP1.append(span);
+    span1.textContent = " " + this.value;
     this.value = "";
   }
 });
 
 // получаем зачение второго инпута и сохраняем в span
-input2.addEventListener("keydown", function (event) {
+input2Task1.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    taskAnswerP2.textContent = "Второе значение: ";
-    let span = document.createElement("span");
-    span.textContent = " " + this.value;
-    span.id = "span2";
-    taskAnswerP2.append(span);
+    span2.textContent = " " + this.value;
     this.value = "";
   }
 });
 
 // получаем зачение третьего инпута и сохраняем в span
-input3.addEventListener("keydown", function (event) {
+input3Task1.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    taskAnswerP3.textContent = "Третье значение: ";
-    let span = document.createElement("span");
-    span.textContent = " " + this.value;
-    span.id = "span3";
-    taskAnswerP3.append(span);
+    span3.textContent = " " + this.value;
     this.value = "";
   }
 });
@@ -108,15 +102,9 @@ takeAnswerBtn1.addEventListener("click", function () {
   const c = Number(span3.textContent);
 
   if (isItPythagoreanTriple(a, b, c) == true) {
-    let spanAnswer = document.createElement("span");
-    spanAnswer.id = "spanAnswer";
     spanAnswer.textContent = "Да, это Пифагоровая тройка";
-    taskAnswerMain.append(spanAnswer);
   } else {
-    let spanAnswer = document.createElement("span");
-    spanAnswer.id = "spanAnswer";
     spanAnswer.textContent = "Нет, это не Пифагровая тройка";
-    taskAnswerMain.append(spanAnswer);
   }
 });
 
@@ -130,51 +118,47 @@ function resetValues() {
 
 buttonReset1.addEventListener("click", resetValues);
 
-input21.addEventListener("keydown", function (event) {
+//******************************************************************************
+//******************************* TASK TWO *************************************
+
+input1Task2.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    let span1Task2 = document.createElement("span");
-    span1Task2.textContent += " " + this.value;
-    span1Task2.id = "span1Task2";
-    taskAnswerMain2.append(span1Task2);
+    span1Task2.textContent = this.value;
     this.value = "";
   }
 });
 
 // функция нахождения делителей числа
 function takeDivisionsofNumb(numb) {
-  let arr = [];
+  let result = "";
   for (let i = 2; i < numb; i++) {
     if (numb % i == 0) {
-      arr.push(i);
+      result += i + "," + " ";
     }
   }
-  let result = "";
-
-  for (let elem of arr) {
-    result += " " + elem + ",";
-  }
-  result = result.slice(0, -1);
+  result = result.slice(0, -2);
   return result;
 }
 
 // console.log(takeDivisionsofNumb(100));
 
 takeAnswerBtn2.addEventListener("click", function () {
-  let p = document.querySelector("p");
-  p.textContent =
-    "Список делителей: " + takeDivisionsofNumb(Number(span1Task2.textContent));
-  parentTaskAnswerZoneTask2.append(p);
+  spanAnswerTask2.textContent = takeDivisionsofNumb(span1Task2.textContent);
 });
+
+buttonReset2.addEventListener("click", function () {
+  span1Task2.textContent = "";
+  spanAnswerTask2.textContent = "";
+  input1Task2.value = "";
+});
+
+//******************************************************************************
+//******************************* TASK THREE ***********************************
 
 // сохраняем значение первого инпута в первый абзац
 input1Task3.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    AnswerP1Task3.textContent = "Первое значение: ";
-    let span31 = document.createElement("span");
     span31.textContent = " " + this.value;
-    span31.id = "span31";
-    AnswerP1Task3.append(span31);
-
     this.value = "";
   }
 });
@@ -182,13 +166,7 @@ input1Task3.addEventListener("keydown", function (event) {
 // сохраняем значение второго инпута во второй абзац
 input2Task3.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    AnswerP2Task3.textContent = "Второе значение: ";
-    let span32 = document.createElement("span");
-    span32.id = "span32";
-    span32.textContent = "";
     span32.textContent = this.value;
-    AnswerP2Task3.append(span32);
-
     this.value = "";
   }
 });
@@ -206,7 +184,9 @@ function takeCommonDivisions(numb1, numb2) {
   return str;
 }
 
-// навешиваем функция на кнопку "узнать ответ"
+// console.log(takeCommonDivisions(100, 200));
+
+// навешиваем функцию на кнопку "узнать ответ"
 takeAnswerBtn3.addEventListener("click", function () {
   taskAnswerMainTask3.textContent =
     "Все общие делители этих двух чисел: " +
@@ -220,16 +200,16 @@ buttonReset3.addEventListener("click", function () {
   taskAnswerMainTask3.textContent = "Все общие делители этих двух чисел: ";
 });
 
-_input1Task4.addEventListener("keydown", function (event) {
+input1Task4.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    _span41.textContent = this.value;
+    span41.textContent = this.value;
     this.value = "";
   }
 });
 
-_input2Task4.addEventListener("keydown", function (event) {
+input2Task4.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    _span42.textContent = this.value;
+    span42.textContent = this.value;
     this.value = "";
   }
 });
@@ -246,19 +226,19 @@ function takeMaxDivision(numb1, numb2) {
 
 // console.log(takeMaxDivision(100, 244));
 
-_takeAnswerBtn4.addEventListener("click", function () {
-  _spanMainAnswer.textContent = takeCommonDivisions(
-    _span41.textContent,
-    _span42.textContent
+takeAnswerBtn4.addEventListener("click", function () {
+  spanMainAnswer.textContent = takeCommonDivisions(
+    span41.textContent,
+    span42.textContent
   );
 });
 
 function reset4() {
-  _input1Task4.value = "";
-  _input2Task4.value = "";
-  _span41.textContent = "";
-  _span42.textContent = "";
-  _spanMainAnswer.textContent = "";
+  input1Task4.value = "";
+  input2Task4.value = "";
+  span41.textContent = "";
+  span42.textContent = "";
+  spanMainAnswer.textContent = "";
 }
 
-_buttonReset4.addEventListener("click", reset4);
+buttonReset4.addEventListener("click", reset4);
